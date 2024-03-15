9e30f64500d1a86a1c6f9d3a06d2bb699ce9f564
Alex Turner <turneral1@grinnell.edu>
2024-03-15 08:35:52 -0700

    Fixed whitespace test

--- projects/10/syntax_tests.py
+++ projects/10/syntax_tests.py
@@ -18,3 +18,20 @@ class TestJackTokenizer:
     def test_trim_whitespace(self, original: str, new: str):
         Parser = syntax_analyzer.JackTokenizer("")
         assert Parser._trim_whitespace(original) == new
+
+    @pytest.mark.parametrize(
+        "original, new",
+        [
+            ("1 + 2", "1 + 2"),
+            ("//Comment", ""),
+            ("/* Comment */", ""),
+            ("/* Comment \n Comment */", ""),
+            ("/* Comment \n Comment */ 1 + 2", " 1 + 2"),
+            ("/* Comment \n Comment */ 1 + 2 /* Comment \n Comment */", " 1 + 2 "),
+            ("1 + 2 // Comment", "1 + 2 "),
+            ("xab/**csfdsa fas\n */", "xab"),
+        ],
+    )
+    def test_remove_comments(self, original: str, new: str):
+        Parser = syntax_analyzer.JackTokenizer("")
+        assert Parser._remove_comments(original) == new
