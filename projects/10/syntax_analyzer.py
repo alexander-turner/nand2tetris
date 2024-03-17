b242e554ebd8ffaa193c9ec3c6aef95a49b143f8
Alex Turner <turneral1@grinnell.edu>
2024-03-17 14:45:17 -0700

    Finished the tokenizer and started on the compilation engine; the expression- and array-free output comparisons are now passing

--- projects/10/syntax_analyzer.py
+++ projects/10/syntax_analyzer.py
@@ -1,9 +1,12 @@
 import enum
 from absl import flags
-from typing import List, Literal, Tuple
+import logging
+from typing import List, Literal, Tuple, Sequence
 import os
 import sys
 import re
+
+logging.basicConfig(level=logging.DEBUG)
 
 FLAGS = flags.FLAGS
 _SOURCE = flags.DEFINE_string(
@@ -41,7 +44,7 @@ _SYMBOLS = (
     "~",
 )
 _INTEGERS = range(32768)
-_IDENTIFIER_SEP: Tuple[str] = (",", "(", "]", ")", ";", "{", " ", ".")
+_SEPARATORS: Tuple[str] = (",", "[", "]", "(", ")", ";", "{", "}", " ", ".")
 
 
 def _is_str_constant(s: str) -> bool:
@@ -70,11 +73,13 @@ class JackTokenizer:
     def __init__(self, lines: list[str] | str):
         """Open the input file/stream and get ready to parse it."""
         if isinstance(lines, str):
-            self.lines = lines.split("\n")
+            lines = lines.split("\n")
 
         # Preprocess lines to remove comments and whitespace
-        text: str = " ".join(self.lines)
+        text: str = " ".join(lines)
         self.remaining_text = self._trim_whitespace(self._remove_comments(text))
+
+        self.tokens: List[str] = []
 
     @staticmethod
     def _remove_comments(original_line: str) -> str:
@@ -106,10 +111,14 @@ class JackTokenizer:
 
     def advance(self):
         """Read the next token from the input and make it the current token."""
+        logging.debug(f"{self.remaining_text=} in advance() call.\n\n")
+
         for token_str in list(_SYMBOLS) + list(Keyword):
             if self.remaining_text.startswith(token_str):
-                self.current_token_str = token_str
+                self.tokens.append(token_str)
                 self.remaining_text = self.remaining_text[len(token_str) :]
+                if self.remaining_text.startswith(" "):
+                    self.remaining_text = self.remaining_text[1:]
                 return
 
         if self.remaining_text[0] == '"':  # String constant
@@ -117,76 +126,126 @@ class JackTokenizer:
             implied_str = self.remaining_text[:next_idx]  # Quote-inclusive
             if not _is_str_constant(implied_str):
                 raise ValueError(f"Invalid string constant: {implied_str}")
-            self.current_token_str = implied_str  # QUESTION should we strip the quotes?
+            self.tokens.append(implied_str)
         elif self.remaining_text[0].isdigit():  # Integer constant
-            next_idx: int = self._index_or_end(" ")
-            implied_int_str: str = self.remaining_text[:next_idx]
+            implied_int_str = self._str_until_separator()
+            next_idx: int = len(implied_int_str)
             if not implied_int_str.isdigit() or int(implied_int_str) not in _INTEGERS:
                 raise ValueError(f"Invalid integer constant: {implied_int_str}")
-            self.current_token_str = implied_int_str
+            self.tokens.append(implied_int_str)
         else:  # Identifier
-            # Find the next separator
-            next_idx = len(self.remaining_text)
-            for sep in _IDENTIFIER_SEP:
-                if sep in self.remaining_text:
-                    next_idx = min(next_idx, self.remaining_text.index(sep))
-            implied_id: str = self.remaining_text[:next_idx]
+            implied_id: str = self._str_until_separator()
+            next_idx: int = len(implied_id)
+            logging.debug(f"{implied_id=} and {next_idx=}\n")
             if not _is_identifier(implied_id):
                 raise ValueError(f"Invalid identifier: {implied_id}")
-            self.current_token_str = implied_id
+            self.tokens.append(implied_id)
         self.remaining_text = self.remaining_text[next_idx:]
+        if self.remaining_text.startswith(" "):
+            self.remaining_text = self.remaining_text[1:]
 
-    def token_type(self) -> TokenType:
+    def _str_until_separator(self) -> str:
+        next_idx = len(self.remaining_text)
+        for sep in _SEPARATORS:
+            if sep in self.remaining_text:
+                next_idx = min(next_idx, self.remaining_text.index(sep))
+        return self.remaining_text[:next_idx]
+
+    def token_type(self, token_str: str) -> TokenType:
         """Returns the type of the current token."""
-        if self.current_token_str in _SYMBOLS:
-            return TerminalTokenType.SYMBOL
-        elif self.current_token_str in Keyword:
-            return TerminalTokenType.KEYWORD
-        elif _is_str_constant(self.current_token_str):
-            return TerminalTokenType.STRING_CONST
-        elif _is_identifier(self.current_token_str):
-            return TerminalTokenType.IDENTIFIER
-        elif (
-            self.current_token_str.isdigit()
-            and int(self.current_token_str) in _INTEGERS
-        ):
-            return TerminalTokenType.INT_CONST
+        if token_str in _SYMBOLS:
+            return TokenType.SYMBOL
+        elif token_str in Keyword:
+            return TokenType.KEYWORD
+        elif _is_str_constant(token_str):
+            return TokenType.STRING_CONST
+        elif _is_identifier(token_str):
+            return TokenType.IDENTIFIER
+        elif token_str.isdigit() and int(token_str) in _INTEGERS:
+            return TokenType.INT_CONST
         else:
-            raise ValueError(f"Unrecognized token: {self.current_token_str}")
+            raise ValueError(f"Unrecognized token: {token_str}")
 
-    def keyword(self) -> str:
+    def keyword(self, token_str: str) -> str:
         """Returns the keyword which is the current token."""
-        if self.token_type() != TerminalTokenType.KEYWORD:
+        if self.token_type(token_str) != TokenType.KEYWORD:
             raise ValueError("Current token is not a keyword.")
-        return Keyword[self.current_token_str.upper()]
+        return Keyword[token_str.upper()]
 
-    def symbol(self) -> str:
+    def symbol(self, token_str: str) -> str:
         """Returns the character which is the current token."""
-        if self.token_type() != TerminalTokenType.SYMBOL:
+        if self.token_type(token_str) != TokenType.SYMBOL:
             raise ValueError("Current token is not a symbol.")
-        return self.current_token_str
+        return token_str
 
-    def identifier(self) -> str:
+    def identifier(self, token_str: str) -> str:
         """Returns the identifier which is the current token."""
-        if self.token_type() != TerminalTokenType.IDENTIFIER:
+        if self.token_type(token_str) != TokenType.IDENTIFIER:
             raise ValueError("Current token is not an identifier.")
-        return self.current_token_str
+        return token_str
 
-    def int_val(self) -> int:
+    def int_val(self, token_str: str) -> int:
         """Returns the integer value of the current token."""
-        if self.token_type() != TerminalTokenType.INT_CONST:
+        if self.token_type(token_str) != TokenType.INT_CONST:
             raise ValueError("Current token is not an integer constant.")
-        return int(self.current_token_str)
+        return int(token_str)
 
-    def string_val(self) -> str:
+    def string_val(self, token_str: str) -> str:
         """Returns the string value of the current token, without the surrounding quotes."""
-        if self.token_type() != TerminalTokenType.STRING_CONST:
+        if self.token_type(token_str) != TokenType.STRING_CONST:
             raise ValueError("Current token is not a string constant.")
-        return self.current_token_str[1:-1]
+        return token_str[1:-1]
+
+    def write_tokens(self, filename: str) -> None:
+        with open(filename, "w") as f:
+            f.write("<tokens>\n")
+            for tok in self.tokens:
+                tok_type = self.token_type(tok)
+                match tok_type:
+                    case TokenType.KEYWORD:
+                        to_write = self.keyword(tok)
+                    case TokenType.SYMBOL:
+                        to_write = self.symbol(tok)
+                    case TokenType.IDENTIFIER:
+                        to_write = self.identifier(tok)
+                    case TokenType.INT_CONST:
+                        to_write = self.int_val(tok)
+                    case TokenType.STRING_CONST:
+                        to_write = self.string_val(tok)
+                    case _:
+                        raise ValueError(f"Unrecognized token: {self.tokens[idx]}")
+                to_write = token_xml(token=to_write, tok_type=tok_type) + "\n"
+                f.write(to_write)
+
+            f.write("\n</tokens>")
 
 
-def token_xml(token: str, type: TokenType) -> str:
-    return f"<{type}> {token} </{type}>"
+def _escape_xml(xml_string: str) -> str:
+    # TODO get escape sequences for " and &
+    conversion_mapping = {"<": "&lt", ">": "&gt", '"': '"', "&": "&"}
+    escaped_str = re.sub(r"<", "&lt", xml_string)
+    escaped_str = re.sub(r">", "&gt", escaped_str)
+    escaped_str = re.sub(r'"', '"', escaped_str)  # TODO
+    return re.sub(r"&", "&", escaped_str)  # TODO
+
+
+def token_xml(token: str, tok_type: TokenType) -> str:
+    return f"<{tok_type}> {token} </{tok_type}>"
+
+
+class CompilationEngine:
+    def __init__(self, input_tokens: Sequence[str], output_filename: str) -> None:
+        self.tokens = input_tokens
+        self.output_filename = output_filename
+
+    def compile_class(self) -> None:
+        """Compiles a complete class."""
+        # 'class' className '{' class_var_dec* subroutine_dec* '}'
+
+        raise NotImplementedError
+
+    def compile_class_var_dec(self) -> None:
+        """Compiles a static variable declaration, or a field declaration."""
 
 
 if __name__ == "__main__":
@@ -206,14 +265,12 @@ if __name__ == "__main__":
         print(f"Analyzing {file}...")
         with open(file, "r") as f:
             lines = f.readlines()
-        tokenizer = JackTokenizer(lines)
 
-        output: str = ""
+        # Clears out whitespace and comments
+        tokenizer = JackTokenizer(lines)
         while tokenizer.has_more_tokens():
-            tokenizer.advance()
-            token: str = tokenizer.current_token_str
-            token_type: TokenType = tokenizer.token_type()
+            tokenizer.advance()  # advance() calls append to tokens
 
         basename = file.partition(".jack")[0]
-        with open(basename + ".xml", "w"):
-            f.write(parsed_lines)
+        xml_name = basename + "-user-T.xml"
+        tokenizer.write_tokens(xml_name)
--- projects/10/syntax_tests.py
+++ projects/10/syntax_tests.py
@@ -1,4 +1,7 @@
 import pytest
+import glob
+import logging
+import re
 import syntax_analyzer
 
 
@@ -59,9 +62,8 @@ class TestJackTokenizer:
         "original, target",
         [
             ("1 + 2", "1"),
-            (" + 2", "+"),
-            (" 2", "2"),
-            ("  1  ", "1"),
+            ("+ 2", "+"),
+            ("2", "2"),
             ("xab ALBERT", "xab"),
             ("5123 31 !!", "5123"),
             ("_FDAV( ", "_FDAV"),
@@ -74,14 +76,86 @@ class TestJackTokenizer:
         tokenizer = syntax_analyzer.JackTokenizer(original)
         tokenizer.advance()
 
-        token_str = tokenizer.current_token_str
+        token_str = tokenizer.tokens[-1]
         assert token_str == target
 
         # Assert the remaining text is as expected
         next_idx: int = len(token_str)
-        assert tokenizer.remaining_text == original.strip()[next_idx:]
+        if len(original) > next_idx and original[next_idx] == " ":
+            next_idx += 1
+        assert (
+            tokenizer.remaining_text == original[next_idx:].rstrip()
+        ), f"{tokenizer.remaining_text=} != {original[next_idx:]=}; {tokenizer.tokens=}"
 
+    @pytest.mark.parametrize(
+        "original, target",
+        [
+            ("1 + 2", "+"),
+            (" + 2", "2"),
+            ("xab ALBERT", "ALBERT"),
+            ("5123 31 !!", "31"),
+            ("_FDAV( ", "("),
+            ("var{", "{"),
+        ],
+    )
+    def test_second_advance(self, original: str, target: str) -> None:
+        tokenizer = syntax_analyzer.JackTokenizer(original)
+        tokenizer.advance()
+        tokenizer.advance()
+
+        token_str = tokenizer.tokens[-1]
+        assert token_str == target
+
+    @pytest.mark.parametrize(
+        "original, tok_type",
+        [
+            ("1", syntax_analyzer.TokenType.INT_CONST),
+            ("+", syntax_analyzer.TokenType.SYMBOL),
+            ("2", syntax_analyzer.TokenType.INT_CONST),
+            ("xab", syntax_analyzer.TokenType.IDENTIFIER),
+            ("5123", syntax_analyzer.TokenType.INT_CONST),
+            ("_FDAV", syntax_analyzer.TokenType.IDENTIFIER),
+            ("var", syntax_analyzer.TokenType.KEYWORD),
+            ('"Hi!"', syntax_analyzer.TokenType.STRING_CONST),
+            *[(s, syntax_analyzer.TokenType.SYMBOL) for s in syntax_analyzer._SYMBOLS],
+        ],
+    )
     def test_token_type(
         self, original: str, tok_type: syntax_analyzer.TokenType
     ) -> None:
-        pass
+        tokenizer = syntax_analyzer.JackTokenizer(original)
+        tokenizer.advance()
+        assert tokenizer.token_type(tokenizer.tokens[-1]) == tok_type
+
+    @pytest.mark.parametrize(
+        "filename",
+        [
+            *glob.glob("Square/*.jack"),
+            *glob.glob("ExpressionLessSquare/*.jack"),
+            *glob.glob("ArrayTest/*.jack"),
+        ],
+    )
+    def test_cmp_written_output(self, filename: str) -> None:
+        with open(filename, "r") as f:
+            lines = f.readlines()
+        tokenizer = syntax_analyzer.JackTokenizer(lines)
+        while tokenizer.has_more_tokens():
+            tokenizer.advance()
+
+        user_generated_filename = filename.replace(".jack", "-user-T.xml")
+        tokenizer.write_tokens(user_generated_filename)
+
+        target_filename = filename.replace(".jack", "T.xml")
+        with open(target_filename, "r") as f:
+            target_text = f.read()
+
+        with open(user_generated_filename, "r") as f:
+            user_text = f.read()
+
+        def _cmp_ignore_whitespace(s1: str, s2: str) -> bool:
+            s1_no_whitespace = re.sub(r"\s+", "", s1)
+            s2_no_whitespace = re.sub(r"\s+", "", s2)
+            return s1_no_whitespace == s2_no_whitespace
+
+        logging.debug(user_text)
+        assert _cmp_ignore_whitespace(target_text, user_text), f"{filename=}"
